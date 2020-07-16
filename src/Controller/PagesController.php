ed126fe80d82c9890c3a6aa0ef912023005af5e9
Nufreort <ryryry203@gmail.com>
2020-07-16 12:23:16 +0200

    trickView

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Tricks;
 use App\Repository\TricksRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class PagesController extends AbstractController
 {
     /**
      * @Route("/", name="app_index")
      */
-    public function index(TricksRepository $tricksRepository)
+    public function index(TricksRepository $tricksRepository): Response
     {
       $tricks = $tricksRepository->findAll();
 
         return $this->render('pages/index.html.twig', compact('tricks'));
+    }
+
+    /**
+     * @Route("/trick/{id}", name="app_trickView")
+     */
+    public function trickView(Tricks $tricks): Response
+    {
+        return $this->render('pages/trickView.html.twig', compact('tricks'));
     }
 }
