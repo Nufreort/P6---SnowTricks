086cf213ea3b84d8164a209792ee356fbb0ec3d9
Nufreort <ryryry203@gmail.com>
2020-07-19 20:46:48 +0200

    user registration

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,16 +2,16 @@
 
 namespace App\Entity;
 
-use App\Repository\UsersRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @ORM\Entity(repositoryClass=UsersRepository::class)
+ * @ORM\Entity(repositoryClass=UserRepository::class)
  * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
  */
-class Users implements UserInterface
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -39,22 +39,17 @@ class Users implements UserInterface
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $photo;
-
-    /**
-     * @ORM\Column(type="datetime")
-     */
-    private $registration_date;
+    private $lastName;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $first_name;
+    private $firstName;
 
     /**
-     * @ORM\Column(type="boolean")
+     * @ORM\Column(type="datetime",options={"default": "CURRENT_TIMESTAMP"})
      */
-    private $isVerified = false;
+    private $registrationDate;
 
     public function getId(): ?int
     {
@@ -134,51 +129,48 @@ class Users implements UserInterface
         // $this->plainPassword = null;
     }
 
-    public function getPhoto(): ?string
+    public function getLastName(): ?string
     {
-        return $this->photo;
+        return $this->lastName;
     }
 
-    public function setPhoto(string $photo): self
+    public function setLastName(string $lastName): self
     {
-        $this->photo = $photo;
-
-        return $this;
-    }
-
-    public function getRegistrationDate(): ?\DateTimeInterface
-    {
-        return $this->registration_date;
-    }
-
-    public function setRegistrationDate(\DateTimeInterface $registration_date): self
-    {
-        $this->registration_date = $registration_date;
+        $this->lastName = $lastName;
 
         return $this;
     }
 
     public function getFirstName(): ?string
     {
-        return $this->first_name;
+        return $this->firstName;
     }
 
-    public function setFirstName(string $first_name): self
+    public function setFirstName(string $firstName): self
     {
-        $this->first_name = $first_name;
+        $this->firstName = $firstName;
 
         return $this;
     }
 
-    public function isVerified(): bool
+    public function getRegistrationDate(): ?\DateTimeInterface
     {
-        return $this->isVerified;
+        return $this->registrationDate;
     }
 
-    public function setIsVerified(bool $isVerified): self
+    public function setRegistrationDate(\DateTimeInterface $registrationDate): self
     {
-        $this->isVerified = $isVerified;
+        $this->registrationDate = $registrationDate;
 
         return $this;
+    }
+
+    /**
+     * @ORM\PrePersist
+     * @ORM\PreUpdate
+     */
+    public function updateTimestamps()
+    {
+          $this->setRegistrationDate(new \DateTimeImmutable);
     }
 }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,7 +2,7 @@
 
 namespace App\Form;
 
-use App\Entity\Users;
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -18,14 +18,8 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
+            ->add('lastName')
+            ->add('firstName')
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
@@ -48,7 +42,7 @@ class RegistrationFormType extends AbstractType
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => Users::class,
+            'data_class' => User::class,
         ]);
     }
 }
